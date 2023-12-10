809d89732d8195777b2908fcd5fcb9f109655496
mdhc_ryz5 <matias.daniel.huentian@gmail.com>
2023-12-10 00:41:24 -0300

    Configurando email

--- config/mail.php
+++ config/mail.php
@@ -103,8 +103,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'prochubware@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Prochubware'),
     ],
 
     /*
